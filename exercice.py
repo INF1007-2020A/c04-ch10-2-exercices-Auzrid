be32c45cbf7fb68dfe112f7c3cedc2bbf3d9f25f
Alexandre Cojot <alexandre.cojot@polymtl.ca>
2020-11-10 19:59:21 -0500

    new
    new

--- exercice.py
+++ exercice.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+import panda as pd
 
 # TODO: Définissez vos fonctions ici
-
+def preprocees_data(data_path: str="https://pandas.pydata.org/docs/"):
+    df =
+    y = df["quality"]
+    X =
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
